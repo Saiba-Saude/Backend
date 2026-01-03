9c9e952fc1ed262838b139e3fcacd70139feb637
unknown <kauasoarres9033@gmail.com>
2026-01-02 21:58:40 -0300

    myslq com a ultima atualização, trocando cadastro por saibamais

--- routes/routes.routes.js
+++ routes/routes.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pacienteController = require("../controlles/pacienteControlles");
 
-router.post("/cadastro", pacienteController.criar);
+router.post("/saibamais", pacienteController.criar);
 router.get("/", pacienteController.listar);
 router.patch("/atualizar/:id", pacienteController.atualizar);
 router.delete("/deletar/:id", pacienteController.deletar);
